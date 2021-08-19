4e0a2ff1e77de9875456b8d5e2fa3d79d4d08c60
Andres Vargas <andres@python3.ninja>
2021-08-19 16:46:52 -0500

    adding readme

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# for install
+## for install
 
 
 ```
 
 pip install netwey-cli
 
-
+```
+## For usage 
+```
 
 export NETWEY_EMAIL=foo@gmail.com
 export NETWEY_PASSWORD=mypassword
 netwey-cli
 
 ```
+
+!()[https://i.imgur.com/y2nymJq.png]
--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ setup(
     name='netwey-cli',
     version='0.1.0',
     py_modules=['netwey'],
+    author="Andres Vargas - zodman",
+    author_email="zodman@gmail.com",
+    long_description=("netwey-cli it fetch the information for your netwey.com.mx"
+                     " account"),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
     install_requires=[
         'Click',
         'bs4',
